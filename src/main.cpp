21d20ee54eedf725a8f803c0e17539f043a5a4f6
B13M4RK <pauldreissig@outlook.com>
2026-02-20 15:04:07 +0100

    test file mit fotowiderstand gemacht um serielle kommunikatin zu testen, funktoniert, man kann auch mit python daten auslesen (anderes repo)

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
 #include <Arduino.h>
 
+int signal;
+
 // put function declarations here:
+int check_light() {
+    signal = analogRead(A15);
+    Serial.println(signal);
+    return signal;
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
+    // put your setup code here, to run once:
+    pinMode(2, OUTPUT);
+    pinMode(A15, INPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+    // put your main code here, to run repeatedly:
+    digitalWrite(2, HIGH);
+    delay(100);
+    check_light();
+    delay(1000);
+    digitalWrite(2, LOW);
+    delay(100);
+    check_light();
+    delay(1000);
 }
